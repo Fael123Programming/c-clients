34275d93cebe7542dacaa27e53b60b420d18b443
Fael123Programming <rafaelfonseca1020@gmail.com>
2023-09-26 13:08:31 -0300

    new commit

--- main.c
+++ main.c
@@ -13,16 +13,23 @@ void registerClient();
 // Prints out an error msg.
 void error(const char*);
 
+// Checks whether user is sure or not for their decision.
+int userIsSure(const char*);
+
+// Reads a client's name and returns it.
+char* readName();
+
+// Reads a client's phone number and returns it.
+char* readPhoneNumber();
+
 int main() {
     loop();
-
     return 0;
 }
 
 void loop() {
     createRecord();
     char option;
-    char exit;
     while (1) {
         printf("-------------------- Clients ---------------------\n");
         printf("(1) - Register\n");
@@ -33,91 +40,94 @@ void loop() {
         scanf("%c", &option);
         fflush(stdin);
         clearTerminal();
-        if (option == '1')
+        if (option == '1') {
             registerClient();
-        else if (option == '2')
+        } else if (option == '2') {
             printf("\n- Search Client -\n");
-        else if (option == '3') {
-            clearTerminal();
-            printf("\n- Exit Clients -\n");
-            printf("Are you sure? [y/n] ");
-            scanf("%c", &exit);
-            fflush(stdin);
-            clearTerminal();
-            if (exit == 'y' || exit == 'Y')
-                return;
-        } else
+        } else if (option == '3') { 
+            if (userIsSure("Clients")) {
+                exit(0);
+            }
+        } else {
             error("\n- Unknown option -\n");
+        }
         clearTerminal();
     }
 }
 
 void registerClient() {
-    char* name = NULL;
-    int nameLen;
-    size_t len = 0;
-    while (1) {
-        printf("\n- Register New Client -\n");
-        printf("%s", drawRow(50, '-'));
-        printf("Name [up to %d chars] ('-1' to decline): ", NAME_SIZE);
-        getline(&name, &len, stdin);
-        strip(name, strlen(name));
-        nameLen = strlen(name);
-        if (isBlank(name, nameLen))
-            error("\n- Name cannot be empty -\n");
-        else {
-            if (nameLen <= NAME_SIZE)
-                break;
-            else
-                error("\n- Name too long -\n");
-        }
-    }
-    if (!strcmp(name, "-1"))
+    char* name = readName();
+    if (!strcmp(name, "-1")) {
         return;
-    char* phoneNumber = NULL;
-    int phoneLen;
-    while (1) {
-        printf("Phone number [up to %d chars] ('-1' to decline): ", PHONE_NUMBER_SIZE);
-        getline(&phoneNumber, &len, stdin);
-        strip(phoneNumber, strlen(phoneNumber));
-        phoneLen = strlen(phoneNumber);
-        if (isBlank(phoneNumber, phoneLen))
-            error("\n- Phone number cannot be empty -\n");
-        else {
-            if (phoneLen <= PHONE_NUMBER_SIZE)
-                break;
-            else
-                error("\n- Phone number too long -\n");
-        }
     }
-    if (!strcmp(phoneNumber, "-1"))
+    clearTerminal();
+    char* phoneNumber = readPhoneNumber();
+    if (!strcmp(phoneNumber, "-1")) {
         return;
+    }
     char opt;
     while (1) {
         clearTerminal();
-        printf("\n - New client -\n");
+        printf("\n - Register client -\n");
         printf("%s", drawRow(50, '-'));
         printf("Name: %s\n", name);
         printf("Phone number: %s\n", phoneNumber);
         printf("%s", drawRow(50, '-'));
-        printf("[1] Create          [2] Edit          [3] Cancel\n");
+        printf("(1) Create          (2) Edit          (3) Cancel\n");
         printf("%s", drawRow(50, '-'));
         printf("-> ");
         scanf("%c", &opt);
         fflush(stdin);
         clearTerminal();
-
         if (opt == '1') {
+            // Write to file.
+            free(name);
+            free(phoneNumber);
             printf("- Created Successfully -\n");
             system("pause");
             break;
         } else if (opt == '2') {
-            printf("- Edit Client -\n");
-            system("pause");
-        } else if (opt == '3')
-            break;
-        else
+            char opt2;
+            while (1) {
+                printf("- Edit Client -\n");
+                printf("%s", drawRow(50, '-'));
+                printf("(1) - Name\n");
+                printf("(2) - Phone number\n");
+                printf("(3) - Exit\n");
+                printf("%s", drawRow(50, '-'));
+                printf("-> ");
+                scanf("%c", &opt2);
+                fflush(stdin);
+                clearTerminal();
+                if (opt2 == '1') {
+                    char* editName = readName();
+                    if (strcmp(editName, "-1")) {
+                        strcpy(name, editName);
+                    }
+                    free(editName);
+                    clearTerminal();
+                } else if (opt2 == '2') {
+                    char* editPhoneNumber = readPhoneNumber();
+                    if (strcmp(editPhoneNumber, "-1")) {
+                        strcpy(phoneNumber, editPhoneNumber);
+                    }
+                    free(editPhoneNumber);
+                    clearTerminal();
+                } else if (opt2 == '3') {
+                    if (userIsSure("Edit")) {
+                        break;
+                    }
+                } else {
+                    error("\n- Unknown option -\n");
+                }
+            }
+        } else if (opt == '3') {
+            if (userIsSure("Register")) {
+                break;
+            }
+        } else {
             error("\n- Unknown option -\n");
+        }
     }
     clearTerminal();
 }
@@ -127,4 +137,64 @@ void error(const char* msg) {
     printf("%s", msg);
     system("pause");
     clearTerminal();
+}
+
+int userIsSure(const char* msg) {
+    int sure = -1;
+    char exit;
+    while (sure == -1) {
+        printf("\n- Exit %s -\n", msg);
+        printf("Are you sure? (y/n) ");
+        scanf("%c", &exit);
+        fflush(stdin);
+        clearTerminal();
+        if (exit == 'y' || exit == 'Y') {
+            sure = 1;
+        } else if (exit == 'n' || exit == 'N') {
+            sure = 0;
+        } else {
+            error("\n- Unknown option -\n");
+        }
+    }
+    return sure;
+}
+
+char* readName() {
+    char* name = NULL;
+    size_t len = 0;
+    while (1) {
+        printf("Name [up to %d chars] ('-1' to decline): ", NAME_SIZE);
+        getline(&name, &len, stdin);
+        if (isBlank(name)) {
+            error("\n- Name cannot be empty -\n");
+        } else {
+            strip(name);
+            if (strlen(name) <= NAME_SIZE) {
+                break;
+            } else {
+                error("\n- Name too long -\n");
+            }
+        }
+    }
+    return name;
+}
+
+char* readPhoneNumber() {
+    char* phoneNumber = NULL;
+    size_t len = 0;
+    while (1) {
+        printf("Phone number [up to %d chars] ('-1' to decline): ", PHONE_NUMBER_SIZE);
+        getline(&phoneNumber, &len, stdin);
+        if (isBlank(phoneNumber)) {
+            error("\n- Phone number cannot be empty -\n");
+        } else {
+            strip(phoneNumber);
+            if (strlen(phoneNumber) <= PHONE_NUMBER_SIZE) {
+                break;
+            } else {
+                error("\n- Phone number too long -\n");
+            }
+        }
+    }
+    return phoneNumber;
 }
--- util.h
+++ util.h
@@ -7,26 +7,25 @@ void clearString(char* str, int size) {
 }
 
 // Takes all leading and trailing spaces out of the given string.
-void strip(char* str, int size) {
+void strip(char* str) {
+    int size = -1;
+    while (str[++size] != '\0');
     int p1 = -1, p2 = -1, i;
     char s, e;
-
     for (i = 0; i < size; i++) {
-        s = *(str + i);
-        e = *(str + (size - 1 - i));
-
+        s = str[i];
+        e = str[size - 1 - i];
         if (p1 == -1 && s != ' ' && s != '\n' && s != '\t')
             p1 = i;
-
         if (p2 == -1 && e != ' ' && e != '\n' && e != '\t' && e != '\0')
             p2 = size - 1 - i;
     }
-
     char* temp = malloc(sizeof(char) * (p2 - p1 + 2));
     for (i = p1; i <= p2; i++)
         temp[i - p1] = str[i];
     temp[i - p1] = '\0';
     strcpy(str, temp);
+    free(temp);
 }
 
 // Clears terminal.
@@ -44,11 +43,11 @@ char* drawRow(int size, char symb) {
     return row;
 }
 
-int isBlank(char* str, int size) {
-    int blank = 1, curr;
-    for (int i = 0; i < size - 1; i++) {
-        curr = *(str + i);
-        if (curr != ' ' && curr != '\n') {
+int isBlank(char* str) {
+    int blank = 1, curr, i;
+    for (i = 0; str[i] != '\0'; i++) {
+        curr = str[i];
+        if (curr != ' ' && curr != '\n' && curr != '\r' && curr != '\0') {
             blank = 0;
             break;
         }
